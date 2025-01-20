aaa237caee3be896991d04e349ae87e46c2cf305
lofidewanto@gmail.com <lofidewanto@gmail.com>
2025-01-20 09:11:51 +0100

    Add NonNull

--- src/main/java/com/github/caac/demo/AiServiceRegisteredEventListener.java
+++ src/main/java/com/github/caac/demo/AiServiceRegisteredEventListener.java
@@ -3,6 +3,7 @@ package com.github.caac.demo;
 import java.util.List;
 
 import org.springframework.context.ApplicationListener;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import dev.langchain4j.agent.tool.ToolSpecification;
@@ -12,7 +13,7 @@ import dev.langchain4j.service.spring.event.AiServiceRegisteredEvent;
 public class AiServiceRegisteredEventListener implements ApplicationListener<AiServiceRegisteredEvent> {
 
     @Override
-    public void onApplicationEvent(AiServiceRegisteredEvent event) {
+    public void onApplicationEvent(@NonNull AiServiceRegisteredEvent event) {
         Class<?> aiServiceClass = event.aiServiceClass();
         List<ToolSpecification> toolSpecifications = event.toolSpecifications();
 
